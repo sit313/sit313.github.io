a8f9922e99f6fb2bc2dee87fb774ca51facfa123
tomluan <sunny.road82@gmail.com>
2015-08-25 11:15:36 +1000

    Upload new page
    I have just uploaded a new homepage

---
+++ includes/mycompany.js
@@ -0,0 +1,8 @@
+/*  Custom JavaScript                                     */
+/*  Written by Chris Converse - http://chrisconverse.com  */
+
+$(document).ready(function(){
+
+	$('.countries li a').attr('data-rel','dialog');
+
+});
